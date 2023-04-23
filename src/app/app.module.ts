3e7a6a90e5b77f74dd0ea904947658cf8f6d1278
i.s.glushkov <i.s.glushkov@tinkoff.ru>
2023-04-23 12:34:46 +0300

    task 2 is in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,13 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import {
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TUI_SANITIZER,
+    TuiLinkModule,
+    TuiSvgModule
+} from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,6 +19,7 @@ import { FooterComponent } from './home/footer/footer.component';
 import { HeaderComponent } from './home/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './home/search/search.component';
+import {RouterLink} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -21,8 +32,8 @@ import { SearchComponent } from './home/search/search.component';
     LoadMoreComponent,
     FooterComponent,
   ],
-  imports: [BrowserModule],
-  providers: [],
+    imports: [BrowserModule, BrowserAnimationsModule, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiLinkModule, RouterLink, TuiSvgModule],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
