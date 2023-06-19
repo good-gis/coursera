3f56f6fee83b97a8e24e68c658b3e86f3c0253d8
i.s.glushkov <i.s.glushkov@tinkoff.ru>
2023-06-19 20:11:57 +0300

    task 4 is almost finished

---
+++ src/app/home/course-list/course/course.component-1.spec.ts
@@ -0,0 +1,70 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { courses } from '../courses-mock';
+import { CourseComponent } from './course.component';
+
+// Test host component
+@Component({
+  template: `
+    <app-course
+      [course]="course"
+      (courseDeleted)="onCourseDeleted($event)"
+    ></app-course>
+  `,
+})
+class TestHostComponent {
+  course: any;
+  courseDeletedEvent = '';
+
+  onCourseDeleted(event: string): void {
+    this.courseDeletedEvent = event;
+  }
+}
+
+describe('CourseComponentWithTestHost', () => {
+  let testHost: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CourseComponent, TestHostComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestHostComponent);
+    testHost = fixture.componentInstance;
+
+    testHost.course = courses[0];
+    fixture.detectChanges();
+  });
+
+  it('should create the test host component', () => {
+    expect(testHost).toBeTruthy();
+  });
+
+  it('should not display anything if no data is provided', () => {
+    testHost.course = undefined;
+    fixture.detectChanges();
+
+    const courseElement = fixture.nativeElement.querySelector('.course');
+
+    expect(courseElement).toBeFalsy();
+  });
+
+  it('should display content if data is provided', () => {
+    const courseElement = fixture.nativeElement.querySelector('.course');
+
+    expect(courseElement).toBeTruthy();
+  });
+
+  it('should emit courseDeleted event when delete button is clicked', () => {
+    const deleteButton = fixture.nativeElement.querySelector('.delete');
+
+    deleteButton.click();
+
+    expect(testHost.courseDeletedEvent).toBe('1');
+  });
+});
--- src/app/home/course-list/course/course.component.spec.ts
+++ src/app/home/course-list/course/course.component.spec.ts
@@ -1,12 +1,15 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { courses } from '../courses-mock';
+import { Course } from './course';
 import { CourseComponent } from './course.component';
 
 describe('CourseComponent', () => {
   let component: CourseComponent;
   let fixture: ComponentFixture<CourseComponent>;
+  const course: Course = courses[0];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,12 +19,62 @@ describe('CourseComponent', () => {
 
     fixture = TestBed.createComponent(CourseComponent);
     component = fixture.componentInstance;
+
+    component.course = course;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
-    component.course = courses[1];
-
-    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should display creation date', () => {
+    const dateElement = fixture.debugElement.query(By.css('.date'));
+
+    expect(dateElement).toBeTruthy();
+  });
+
+  it('should display description', () => {
+    const descriptionElement = fixture.debugElement.query(
+      By.css('.description')
+    );
+
+    expect(descriptionElement).toBeTruthy();
+  });
+
+  it('should display course title', () => {
+    const titleElement = fixture.debugElement.query(By.css('.title'));
+
+    expect(titleElement).toBeTruthy();
+  });
+
+  it('should display course duration badge', () => {
+    const durationElement = fixture.debugElement.query(By.css('.duration'));
+
+    expect(durationElement).toBeTruthy();
+  });
+
+  it('should display "Edit" button', () => {
+    const editButton = fixture.debugElement.query(By.css('.edit'));
+
+    expect(editButton).toBeTruthy();
+  });
+
+  it('should display "Delete" button', () => {
+    const deleteButton = fixture.debugElement.query(By.css('.delete'));
+
+    expect(deleteButton).toBeTruthy();
+  });
+
+  it('should emit course id when "Delete" button is clicked', () => {
+    jest.spyOn(component.courseDeleted, 'emit');
+
+    const deleteButton = fixture.debugElement.query(
+      By.css('.delete')
+    ).nativeElement;
+
+    deleteButton.click();
+
+    expect(component.courseDeleted.emit).toHaveBeenCalledWith(course.id);
   });
 });
