ae0ad7f4ff23216e4aa156e8a20fd71caf87051d
i.s.glushkov <i.s.glushkov@tinkoff.ru>
2023-07-17 21:00:24 +0300

    all is done

--- src/app/login-page/login-page.module.ts
+++ src/app/login-page/login-page.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TuiButtonModule, TuiTextfieldControllerModule } from "@taiga-ui/core";
+import { TuiInputModule, TuiInputPasswordModule } from "@taiga-ui/kit";
 
 import { FooterModule } from "../footer/footer.module";
 import { LoginPageComponent } from "./login-page/login-page.component";
@@ -8,6 +10,15 @@ import { LoginPageComponent } from "./login-page/login-page.component";
 @NgModule({
     declarations: [LoginPageComponent],
     exports: [LoginPageComponent],
-    imports: [CommonModule, FooterModule, FormsModule],
+    imports: [
+        CommonModule,
+        FooterModule,
+        FormsModule,
+        TuiInputPasswordModule,
+        ReactiveFormsModule,
+        TuiInputModule,
+        TuiTextfieldControllerModule,
+        TuiButtonModule,
+    ],
 })
 export class LoginPageModule {}
